a28b74626eeb566e11c5c7810903b59ee3d45222
bekii-salkic <Bernat.salkic@hotmail.com>
2025-05-14 20:20:45 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+body {
+  font-family: sans-serif;
+  text-align: center;
+}
+table {
+  margin: auto;
+  border-collapse: collapse;
+}
+td {
+  width: 50px;
+  height: 50px;
+  border: 1px solid #999;
+  font-size: 20px;
+  cursor: pointer;
+}
+td.marked {
+  background-color: lightgreen;
+}
+button {
+  font-size: 18px;
+  padding: 10px 20px;
+  margin: 20px;
+}
